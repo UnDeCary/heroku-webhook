70b3ea25d001b6866e503e7e6d3508e550fe8f83
UnDeCary <100521852+UnDeCary@users.noreply.github.com>
2022-02-27 20:11:24 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,39 @@
+import os
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from flask import Flask, request
+
+bot = Bot(token = '1646981030:AAGYMP-_SjuqJqhJguI9GccCAv2Wso6hQnE')
+dp = Dispatcher('https://git.heroku.com/undecary-heroku.git')
+
+server = Flask(__name__)
+
+@dp.message_handler(commands=['start'])
+async def hi(message: types.Message):
+    await bot.send_message(message.from_user.id, f'Hi {message.from_user.first_name}')
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.from_user.id, message.text)
+
+
+@server.route('/' + TOKEN, methods = ['POST'])
+def get_message():
+    json.string = request.get_data().decode('utf-8')
+    update = types.Update.de_json(json.string)
+    dp.process_updates([update])
+    return '!', 200
+
+
+
+@server.route('/')
+def webhook():
+    bot.delete_webhook()
+    bot.set_webhook(url = APP_URL)
+    return '!', 200
+
+
+
+
+if __name__=='__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
